c12566c913eab96213fedf27dd4047a22468a935
xbomx <49806281+xbomx@users.noreply.github.com>
2021-06-25 15:52:17 +0700

    Update cek.sh

--- cek.sh
+++ cek.sh
@@ -3,3 +3,7 @@ sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RXQKctiyjiVXGnqo9ZEaTZZtMT1B1QX1sj.kerja -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
